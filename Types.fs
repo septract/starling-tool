f0f571fc373f3ed61e9d078a09845924f9b296fc
Matt Windsor <mattwindsor@btinternet.com>
2015-10-31 21:24:31 +0000

    Format and comment types.

--- Types.fs
+++ Types.fs
@@ -2,62 +2,83 @@
 
 [<AutoOpen>]
 module Types =
+    /// An expression.
+    /// These currently cover all languages, but this may change later.
     type Expression =
-        | TrueExp
-        | FalseExp
-        | IntExp of int64
-        | IdExp of string
-        | MulExp of Expression * Expression
-        | DivExp of Expression * Expression
-        | AddExp of Expression * Expression
-        | SubExp of Expression * Expression
-        | GtExp of Expression * Expression
-        | GeExp of Expression * Expression
-        | LeExp of Expression * Expression
-        | LtExp of Expression * Expression
-        | EqExp of Expression * Expression
-        | NeqExp of Expression * Expression
-        | AndExp of Expression * Expression
-        | OrExp of Expression * Expression
+        | TrueExp                             // true
+        | FalseExp                            // false
+        | IntExp   of int64                   // 42
+        | IdExp    of string                  // foobaz
+        | MulExp   of Expression * Expression // a * b
+        | DivExp   of Expression * Expression // a / b
+        | AddExp   of Expression * Expression // a + b
+        | SubExp   of Expression * Expression // a - b
+        | GtExp    of Expression * Expression // a > b
+        | GeExp    of Expression * Expression // a >= b
+        | LeExp    of Expression * Expression // a < b
+        | LtExp    of Expression * Expression // a <= b
+        | EqExp    of Expression * Expression // a == b
+        | NeqExp   of Expression * Expression // a != b
+        | AndExp   of Expression * Expression // a && b
+        | OrExp    of Expression * Expression // a || b
 
+    /// A mode for the Fetch atomic action.
+    type FetchMode =
+        | Direct    // <a = b>
+        | Increment // <a = b++>
+        | Decrement // <a = b-->
+
+    /// An atomic action.
+    type Atomic =
+        | CompareAndSwap of string * Expression * Expression // <CAS(a, b, c)>
+        | Fetch of string * string * FetchMode               // <a = b??>
+
+    /// A view.
     type View =
+        // TODO(CaptainHayashi): fold Apply and NamedView into one thing?
         | Apply of View * args: string list
         | NamedView of string
         | Unit
         | Join of View * View
         | IfView of Expression * View * View
 
+    /// A statement in the command language.
     type Command =
-        | Atomic of string
-        | Skip
-        | If of Expression * Block * Block
-        | While of Expression * Block
-        | DoWhile of Block * Expression
-        | Blocks of Block list
+        | Atomic  of string                     // <a := b++>;
+        | Skip                                  // ;
+        | If      of Expression * Block * Block // if (e) { t } { f }
+        | While   of Expression * Block         // while (e) { b }
+        | DoWhile of Block * Expression         // do { b } while (e)
+        | Blocks  of Block list                 // { a } || { b } || { c }
 
+    /// A combination of a command and its postcondition view.
     and ViewedCommand =
         {
-            Command : Command
-            Post    : View
+            Command : Command // <a := b++;
+            Post    : View    // {| a = b |}
         }
 
+    /// A block or method body.
     and Block = {
         Pre      : View
         // Post-condition is that in the last Seq.
         Contents : ViewedCommand list
     }
 
+    /// A constraint, binding a view to an expression.
     type Constraint = {
         CView       : View
         CExpression : Expression
     }
 
+    /// A method.
     type Method = {
-        Name   : string
-        Params : string list
-        Body   : Block
+        Name   : string      // main ...
+        Params : string list //      ... (argv, argc) ...
+        Body   : Block       //                       ... { ... }
     }
 
+    /// A top-level item in a Starling script.
     type ScriptItem =
-        | SMethod of Method
-        | SConstraint of Constraint
+        | SMethod of Method         // method main(argv, argc) { ... }
+        | SConstraint of Constraint // constraint emp => true
