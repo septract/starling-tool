e43c29c3ff18c223c946de5ecf819065b9df99c8
Matt Windsor <mbw500@york.ac.uk>
2015-11-02 17:26:47 +0000

    Use expr pretty-printer for tests.

--- ParserTests.fs
+++ ParserTests.fs
@@ -5,41 +5,42 @@ open FParsec
 open Starling
 
 module Parser =
-    /// Assertion that parsing `expr` with parser `pp` gets the AST `ast`.
-    let assertParse pp msg expr ast =
-        Assert.Equal ( msg,
-                       Some ast,
-                       match ( run pp expr ) with
+    /// Assertion that parsing `concrete` with parser `pp` gets the AST `ast`.
+    let assertParse pp msg concrete ast =
+        Assert.Equal ( msg
+                     , Some ast
+                     , match ( run pp concrete ) with
                            | Success ( result, _, _ ) -> Some result
                            | Failure _                -> None
                      )
 
+    /// Assertion that parsing expression `expr` with parser `pp` gets the AST `ast`.
+    let assertParseExpr expr ast =
+        assertParse Parser.parseExpression
+                    ( expr + " -> " + Pretty.printExpression ast )
+                    expr
+                    ast
+
     let testExpressionPrecedence =
         testList "Test that parsing expressions yields correct precedence" [
             testCase "1 + 2 * 3 -> 1 + (2 * 3)" <|
-                fun _ -> assertParse Parser.parseExpression
-                                     "1 + 2 * 3 -> 1 + (2 * 3)"
-                                     "1 + 2 * 3"
-                                     ( AddExp ( IntExp 1L, MulExp ( IntExp 2L, IntExp 3L ) ) )
+                fun _ -> assertParseExpr "1 + 2 * 3"
+                                         ( AddExp ( IntExp 1L, MulExp ( IntExp 2L, IntExp 3L ) ) )
             testCase "(1 + 2) * 3 -> (1 + 2) * 3" <|
-                fun _ -> assertParse Parser.parseExpression
-                                     "(1 + 2) * 3 -> (1 + 2) * 3"
-                                     "(1 + 2) * 3"
-                                     ( MulExp ( AddExp ( IntExp 1L, IntExp 2L ), IntExp 3L ) )
+                fun _ -> assertParseExpr "(1 + 2) * 3"
+                                         ( MulExp ( AddExp ( IntExp 1L, IntExp 2L ), IntExp 3L ) )
             testCase "1 + 2 < 3 * 4 && true || 5 / 6 > 7 - 8 -> ((1 + 2) < (3 * 4) && true) || ((5 / 6) > (7 - 8))" <|
-                fun _ -> assertParse Parser.parseExpression
-                                     "1 + 2 < 3 * 4 && true || 5 / 6 > 7 - 8 -> ((1 + 2) < (3 * 4) && true) || ((5 / 6) > (7 - 8))"
-                                     "1 + 2 < 3 * 4 && true || 5 / 6 > 7 - 8"
-                                     ( OrExp ( AndExp ( LtExp ( AddExp ( IntExp 1L, IntExp 2L )
-                                                              , MulExp ( IntExp 3L, IntExp 4L )
-                                                              )
-                                                      , TrueExp
-                                                      )
-                                             , GtExp ( DivExp ( IntExp 5L, IntExp 6L )
-                                                     , SubExp ( IntExp 7L, IntExp 8L )
-                                                     )
-                                             )
-                                     )
+                fun _ -> assertParseExpr "1 + 2 < 3 * 4 && true || 5 / 6 > 7 - 8"
+                                         ( OrExp ( AndExp ( LtExp ( AddExp ( IntExp 1L, IntExp 2L )
+                                                                  , MulExp ( IntExp 3L, IntExp 4L )
+                                                                  )
+                                                          , TrueExp
+                                                          )
+                                                 , GtExp ( DivExp ( IntExp 5L, IntExp 6L )
+                                                         , SubExp ( IntExp 7L, IntExp 8L )
+                                                         )
+                                                 )
+                                         )
         ]
 
     let testAtomicFetch =
