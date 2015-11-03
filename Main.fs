7e71d1eaf2c99abf6e70023fe78f09a3aa23288f
Matt Windsor <mbw500@york.ac.uk>
2015-11-03 18:52:04 +0000

    Add full pretty printer.

--- Main.fs
+++ Main.fs
@@ -11,13 +11,16 @@ type Options = {
     [<Option('t',
              HelpText = "Ignore input and run regression tests.")>]
     test: bool;
+    [<Option('p',
+             HelpText = "Pretty-prints the input instead of attempting to verify it.")>]
+    pprint: bool;
     [<Value(0,
             MetaName = "input",
             HelpText = "The file to load (omit, or supply -, for standard input).")>]
     input: string option;
 }
 
-let parseFile name =
+let parseFile name pprint =
     let (stream, streamName) =
         match name with
             | Some("-") -> (Console.OpenStandardInput(),        "(stdin)")
@@ -25,7 +28,9 @@ let parseFile name =
             | Some(nam) -> (IO.File.OpenRead(nam) :> IO.Stream, nam      )
     let pres = runParserOnStream Starling.Parser.parseScript () streamName stream Text.Encoding.UTF8
     match pres with
-        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Success(result, _, _)   -> if pprint
+                                     then printfn "%s" ( Starling.Pretty.printScript result )
+                                     else printfn "Success: %A" result
         | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
     0
@@ -33,7 +38,7 @@ let parseFile name =
 let mainWithOptions opts argv =
     if opts.test
     then defaultMainThisAssembly argv
-    else parseFile opts.input
+    else parseFile opts.input opts.pprint
 
 [<EntryPoint>]
 let main argv =
--- Pretty.fs
+++ Pretty.fs
@@ -29,3 +29,75 @@ module Pretty =
             | OrExp    (a, b) -> printBinop a "||" b
     /// Pretty-prints binary operations.
     and printBinop a o b = "(" + printExpression a + o + printExpression b + ")"
+
+    /// Pretty-prints argument lists.
+    let printArgList ss = "(" + String.concat ", " ss + ")"
+
+    /// Pretty-prints views.
+    let rec printView v =
+        match v with
+            | Apply     ( vv,  xs ) -> printView vv + printArgList xs
+            | NamedView s           -> s
+            | Unit                  -> "emp"
+            | Join      ( l, r )    -> printView l + " * " + printView r
+            | IfView    ( e, l, r ) -> "if " + printExpression e + " then " + printView l + " else " + printView r
+
+    /// Pretty-prints view lines.
+    let printViewLine vl = "{| " + printView vl + " |}"
+
+    /// Pretty-prints constraints.
+    let printConstraint cs =
+        "constraint " + printView cs.CView + " => " + printExpression cs.CExpression + ";"
+
+    /// Pretty-prints fetch modes.
+    let printFetchMode m =
+        match m with
+            | Direct    -> ""
+            | Increment -> "++"
+            | Decrement -> "--"
+
+    /// Pretty-prints atomic actions.
+    let printAtomicAction atom =
+        match atom with
+            | CompareAndSwap ( l, f, t ) -> "CAS(" + printLValue l + ", " + printExpression f + ", " + printExpression t
+            | Fetch          ( l, r, m ) -> printLValue l + " = " + printLValue r + printFetchMode m
+            | Postfix        ( l, m )    -> printLValue l + printFetchMode m
+
+    /// Enters a new line at the given indent level.
+    let lnIndent level = "\n" + new string ( ' ', level * 4 )
+
+    /// Pretty-prints commands with the given indent level (in spaces).
+    let rec printCommand level cmd =
+        match cmd with
+            | Atomic  a           -> "<" + printAtomicAction a + ">;"
+            | Skip                -> ";"
+            | If      ( c, t, f ) -> "if (" + printExpression c + ") " + printBlock level t + " " + printBlock level f
+            | While   ( c, b )    -> "while (" + printExpression c + ") " + printBlock level b
+            | DoWhile ( b, c )    -> "do " + printBlock level b + " while (" + printExpression c + ")"
+            | Blocks  bs          -> List.map ( printBlock level ) bs |> String.concat " || "
+            | Assign  ( l, r )    -> printLValue l + " = " + printExpression r + ";"
+    /// Pretty-prints viewed commands with the given indent level (in spaces).
+    and printViewedCommand level vcom =
+        printCommand level vcom.Command + lnIndent level + printViewLine vcom.Post
+    /// Pretty-prints blocks with the given indent level (in spaces).
+    and printBlock level block =
+        "{" + lnIndent ( level + 1 ) + printViewLine block.Pre
+            + lnIndent ( level + 1 ) + ( List.map ( printViewedCommand ( level + 1 ) ) block.Contents
+                                         |> String.concat ( lnIndent ( level + 1 ) )
+                                       )
+            + lnIndent level + "}"
+
+    /// Pretty-prints methods.
+    let printMethod meth =
+        "method " + meth.Name
+                  + " " + printArgList meth.Params
+                  + " " + printBlock 0 meth.Body
+
+    /// Pretty-prints script lines.
+    let printScriptLine sl =
+        match sl with
+            | SMethod     m -> printMethod m
+            | SConstraint c -> printConstraint c
+
+    /// Pretty-prints scripts.
+    let printScript = List.map printScriptLine >> String.concat "\n"
