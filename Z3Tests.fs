43c2d5dd05f78b822c1e15404c4c7099281f2ba6
Matt Windsor <mbw500@york.ac.uk>
2015-11-05 18:14:28 +0000

    Add rudimentary Z3 tests.
    These, of course, need adding to.

---
+++ Z3Tests.fs
@@ -0,0 +1,69 @@
+namespace Starling.Tests
+
+open Fuchu
+open Microsoft.Z3
+open Starling
+
+module Z3 =
+    /// Assertion that converting the arithmetic expression `expr` to Z3
+    /// yields the given AST.
+    let assertZ3ArithExpr ctx expr z3 =
+        Assert.Equal ( Starling.Pretty.printExpression expr
+                       + " -Z3-> " + z3.ToString ()
+                     , Starling.Z3.arithExprToZ3 ctx expr
+                     , Starling.Z3.Arith z3
+                     )
+
+    /// Assertion that converting the Boolean expression `expr` to Z3
+    /// yields the given AST.
+    let assertZ3BoolExpr ctx expr z3 =
+        Assert.Equal ( Starling.Pretty.printExpression expr
+                       + " -Z3-> " + z3.ToString ()
+                     , Starling.Z3.boolExprToZ3 ctx expr
+                     , Starling.Z3.Bool z3
+                     )
+
+    let testExprToZ3 =
+        let ctx = new Context ()
+        let t =
+            testList "Test translation of expressions" [
+                testList "Test translation of arithmetic expressions" [
+                    testCase "Test arithmetic-only expressions" <|
+                        fun _ -> assertZ3ArithExpr ctx
+                                          ( AddExp ( MulExp ( IntExp 1L
+                                                            , IntExp 2L
+                                                            )
+                                                   , IntExp 3L
+                                                   )
+                                          )
+                                          ( ctx.MkAdd [| ctx.MkMul [| ctx.MkInt 1 :> ArithExpr
+                                                                    ; ctx.MkInt 2 :> ArithExpr
+                                                                   |]
+                                                       ; ctx.MkInt 3 :> ArithExpr
+                                                      |]
+                                          )
+                ]
+                testList "Test translation of Boolean expressions" [
+                    testCase "Test Boolean-only expressions" <|
+                        fun _ -> assertZ3BoolExpr ctx
+                                         ( AndExp ( OrExp ( TrueExp
+                                                          , TrueExp
+                                                          )
+                                                  , FalseExp
+                                                  )
+                                         )
+                                         ( ctx.MkAnd [| ctx.MkOr [| ctx.MkTrue ()
+                                                                  ; ctx.MkTrue ()
+                                                                 |]
+                                                      ; ctx.MkFalse ()
+                                                     |])
+                ]
+            ]
+        ctx.Dispose ()
+        t
+
+    [<Tests>]
+    let testZ3 =
+        testList "Test the Z3 translation" [
+            testExprToZ3
+        ]
