60f81d00d7acdea3a2d7358f6e0ab8d08f77cde9
Matt Windsor <mbw500@york.ac.uk>
2016-01-06 10:07:49 +0000

    Replace == with =.
    As far as I can tell, the two are identical here, and = is the
    more standard form (dash and various other shells don't seem to
    accept == in test).

--- countFails.sh
+++ countFails.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Usage: countFails.sh path/to/cvf
 
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   STARLING=mono ./bin/Debug/starling.exe
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
   STARLING=mono ./bin/Debug/starling.exe
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
   STARLING=./bin/Debug/starling.exe
 fi
 
