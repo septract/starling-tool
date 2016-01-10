0e4381cb6b210c18ecfaab67ddc4c1667b18ed89
Matt Windsor <mbw500@york.ac.uk>
2016-01-10 17:07:57 +0000

    Add some constraint parser tests.
    This is mainly in aid of the incoming constraint = ? syntax.

--- ParserTests.fs
+++ ParserTests.fs
@@ -22,7 +22,6 @@ type ParserTests() =
     /// Test cases for testing the expression parser.
     static member ExpressionParses = 
         [ TestCaseData("1 + 2 * 3").Returns(Some(Bop(Add, Int 1L, Bop(Mul, Int 2L, Int 3L))))
-          
           TestCaseData("(1 + 2) * 3").Returns(Some(Bop(Mul, Bop(Add, Int 1L, Int 2L), Int 3L)))
           
           TestCaseData("1 + 2 < 3 * 4 && true || 5 / 6 > 7 - 8")
@@ -48,12 +47,31 @@ type ParserTests() =
           TestCaseData("foo = bar--").Returns(Some(Fetch(LVIdent "foo", LV(LVIdent "bar"), Decrement)))
           TestCaseData("CAS(foo, bar, 2)").Returns(Some(CompareAndSwap(LVIdent "foo", LVIdent "bar", Int 2L))) ]
         |> List.map (fun d -> d.SetName(sprintf "Parse %A" d.OriginalArguments.[0]))
-
+    
     [<TestCaseSource("AtomicParses")>]
     /// Tests whether the expression parser works correctly.
     member x.``the atomics parser parses test case atomics correctly`` expr = 
         expr
         |> run parseAtomic
+        |> ParserTests.ParseResultToOptional
+    
+    /// Test cases for testing the constraint parser.
+    static member ConstraintParses = 
+        [ TestCaseData("constraint emp -> true;").Returns(Some { CView = ViewDef.Unit
+                                                                 CExpression = True })
+          
+          TestCaseData("constraint Func(a, b) -> c > a + b;")
+              .Returns(Some { CView = 
+                                  ViewDef.Func { Name = "Func"
+                                                 Params = [ "a"; "b" ] }
+                              CExpression = Bop(Gt, LV(LVIdent "c"), Bop(Add, LV(LVIdent "a"), LV(LVIdent "b"))) }) ]
+        |> List.map (fun d -> d.SetName(sprintf "Parse %A" d.OriginalArguments.[0]))
+    
+    [<TestCaseSource("ConstraintParses")>]
+    /// Tests whether the constraint parser works correctly.
+    member x.``the constraint parser parses valid constraints correctly`` cs = 
+        cs
+        |> run parseConstraint
         |> ParserTests.ParseResultToOptional
     
     /// Test cases for testing the full parser.
