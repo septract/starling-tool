cddc6dc5cf33f08eb825ac9232d439314741c2e6
Matt Windsor <mbw500@york.ac.uk>
2016-01-11 10:23:09 +0000

    Update regression tests and their runner.
    This is to add the new files, to begin with.
    
    In addition, regress.sh now sorts before comparing.  This should
    eliminate some false positives in the future.

--- regress.sh
+++ regress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Runs test.sh and diffs against testresults.
 # Can be used as a very basic regression test.
-./test.sh | dos2unix| diff -i -b testresults -
+./test.sh | dos2unix | sort | diff -i -b testresults -
