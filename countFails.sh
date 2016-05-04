22d00828712300606f271e7b9163cfd2a241d00b
Matthew Parkinson <parky@outlook.com>
2016-05-04 17:44:42 +0100

    Possible fix to #38.

--- countFails.sh
+++ countFails.sh
@@ -2,7 +2,8 @@
 # Usage: countFails.sh path/to/cvf
 
 printf "%s: " "$1"
-./starling.sh $1 |
-	grep "fail" |
-	cut -d":" -f1 |
-	xargs
+FOO=`./starling.sh $1 |
+	   grep "fail" |
+	   cut -d":" -f1 |
+	   xargs`
+echo $FOO
