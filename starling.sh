e4fc27263e83d23a7b400abaa86e9b0c893ef623
Matthew Parkinson <parky@outlook.com>
2016-05-04 10:04:01 +0100

    Fixed scripts to work with 64bit Cygwin.

--- starling.sh
+++ starling.sh
@@ -6,4 +6,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
   mono ./bin/Debug/starling.exe $*
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
   ./bin/Debug/starling.exe $*
+elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]; then
+  ./bin/Debug/starling.exe $*
 fi
