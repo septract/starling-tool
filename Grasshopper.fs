c471d2d456978358bf909c47f407655c9417ca22
Mike Dodds <mike.dodds@gmail.com>
2016-07-27 14:10:20 +0100

    wrote skeleton file for grasshopper backend.

---
+++ Grasshopper.fs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+///     Backend for emitting verification conditions in Grasshopper format
+/// </summary>
+module Starling.Backends.Grasshopper 
+
+open Chessie.ErrorHandling
+open Starling.Collections
+open Starling.Utils
+open Starling.Core.TypeSystem
+open Starling.Core.Var
+open Starling.Core.Expr
+// open Starling.Core.Sub
+open Starling.Core.Model
+open Starling.Core.Instantiate
+open Starling.Core.GuardedView
+
+[<AutoOpen>] 
+module Types =
+    type Query = unit  
+    type Error = unit 
+
+module Pretty = 
+    let printQuery g = 
+        failwith "not implemented yet" 
+
+    let printGrassError e = 
+        failwith "not implemented yet" 
+
+let grassModel i = 
+    failwith "not implemented yet" 
