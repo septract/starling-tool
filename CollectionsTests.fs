bf324c6a62a858bd5110b7c1eab2136b5d09283f
Ben Simner <benjsimner@gmail.com>
2016-09-02 15:46:11 +0100

    Move collections' unittests over to new file

---
+++ CollectionsTests.fs
@@ -0,0 +1,84 @@
+/// <summary>
+///     Collections used in Starling.
+/// </summary>
+module Starling.Tests.Collections
+
+open Starling.Collections
+
+/// <summary>
+///     Tests for collections.
+/// </summary>
+module Tests =
+    open NUnit.Framework
+
+    module Multiset =
+
+        module TestOfFlatList =
+            let check flatList lst =
+                let mset = Map.ofList lst |> MSet
+                Assert.AreEqual (Multiset.ofFlatList flatList, mset)
+
+            [<Test>]
+            let ``Multiset.ofFlatList creates empty multiset`` () =
+                check [] []
+
+            [<Test>]
+            let ``Multiset.ofFlatList multiple values`` () =
+                check [1; 1] [ (1, 2) ]
+
+            [<Test>]
+            let ``Multiset.ofFlatList multiple keys`` () =
+                check [1; 1; 2; 5; 2; 5; 7; 1]
+                    <| [ (1, 3)
+                         (2, 2)
+                         (5, 2)
+                         (7, 1) ]
+
+
+            module TestToFlatList =
+                let check lst flatList =
+                    let mset = Map.ofList lst |> MSet
+                    Assert.AreEqual(flatList, Multiset.toFlatList mset)
+
+                [<Test>]
+                let ``Multiset.toFlatList empty`` () =
+                    check [] []
+
+                [<Test>]
+                let ``Multiset.toFlatList singleton`` () =
+                    check [ (1, 1) ]
+                          [ 1 ]
+
+                [<Test>]
+                let ``Multiset.toFlatList multiple`` () =
+                    check [ (1, 2) ]
+                          [ 1; 1 ]
+
+                [<Test>]
+                let ``Multiset.toFlatList repeated`` () =
+                    check [ (1, 2); ]
+                          [ 1; 1; ]
+
+                [<Test>]
+                let ``Multiset.toFlatList big`` () =
+                    check [ (1, 2); (3, 1); (4, 5); ]
+                          [ 1; 1; 3; 4; 4; 4; 4; 4; ]
+
+        module TestMultisetAppend =
+            let check inputMapList appendList outputMapList =
+                let mset = MSet <| Map.ofList inputMapList
+                let finalMSet = MSet <| Map.ofList outputMapList
+                let appendMset = Multiset.ofFlatList appendList
+                Assert.AreEqual(Multiset.append mset appendMset, finalMSet)
+
+            let ``Multiset.append empty lhs`` () =
+                check [] [1] [ (1, 1) ]
+
+            let ``Multiset.append empty rhs`` () =
+                check [ (1, 1) ] [] [ (1, 1) ]
+
+            let ``Multiset.append increment`` () =
+                check [ (1, 1) ] [1] [ (1, 2) ]
+
+            let ``Multiset.append big`` () =
+                check [ (1, 2); (1, 1); (3, 4) ] [1] [ (1, 3); (1, 1); (3, 4) ]
