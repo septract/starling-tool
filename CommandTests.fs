3ef98288429c1332288e1ac1c370d01d7c083ca4
Matt Windsor <mbw500@york.ac.uk>
2016-12-07 10:41:18 +0000

    Command: remove dead composition code.

--- CommandTests.fs
+++ CommandTests.fs
@@ -9,7 +9,6 @@ open Starling.Utils.Testing
 
 open Starling.Core.Command
 open Starling.Core.Command.Create
-open Starling.Core.Command.Compose
 open Starling.Core.Model
 open Starling.Core.Var
 open Starling.Core.Symbolic
@@ -58,79 +57,3 @@ module Assumes =
     let ``Reject baz <- Foo(bar); Assume(x!before) as an Assume`` ()=
         checkNot [ command "Foo" [ normalIntExpr (siVar "baz") ] [ normalIntExpr (siVar "bar") ]
                    command "Assume" [] [ normalBoolExpr (sbVar "x") ] ]
-
-
-    let checkIntermediate v i e = assertEqual i (getIntermediate v e)
-
-    [<Test>]
-    let ``getIntermediate on given Bool intermediate returns its intermediate``() =
-        checkIntermediate "foo" (Some 5I) (normalBoolExpr (sbInter 5I "foo"))
-
-    [<Test>]
-    let ``getIntermediate on given Bool before returns nothing``() =
-        checkIntermediate "foo" None (normalBoolExpr (sbBefore "foo"))
-
-    [<Test>]
-    let ``getIntermediate on given Bool after returns nothing``() =
-        checkIntermediate "foo" None (normalBoolExpr (sbAfter "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Bool intermediate returns nothing``() =
-        checkIntermediate "bar" None (normalBoolExpr (sbInter 5I "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Bool before returns nothing``() =
-        checkIntermediate "bar" None (normalBoolExpr (sbBefore "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Bool after returns nothing``() =
-        checkIntermediate "bar" None (normalBoolExpr (sbAfter "foo"))
-
-    [<Test>]
-    let ``getIntermediate on given Int intermediate returns its intermediate``() =
-        checkIntermediate "foo" (Some 10I) (normalIntExpr (siInter 10I "foo"))
-
-    [<Test>]
-    let ``getIntermediate on given Int before returns nothing``() =
-        checkIntermediate "foo" None (normalIntExpr (siBefore "foo"))
-
-    [<Test>]
-    let ``getIntermediate on given Int after returns nothing``() =
-        checkIntermediate "foo" None (normalIntExpr (siAfter "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Int intermediate returns nothing``() =
-        checkIntermediate "bar" None (normalIntExpr (siInter 5I "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Int before returns nothing``() =
-        checkIntermediate "bar" None (normalIntExpr (siBefore "foo"))
-
-    [<Test>]
-    let ``getIntermediate on other Int after returns nothing``() =
-        checkIntermediate "bar" None (normalIntExpr (siAfter "foo"))
-
-    [<Test>]
-    let ``getIntermediate on 'not' passes through``() =
-        checkIntermediate "bar" (Some 10I) (normalBoolExpr (BNot (sbInter 10I "bar")))
-
-    [<Test>]
-    let ``getIntermediate on 'implies' is max of its arguments matching the name``() =
-        checkIntermediate "a" (Some 6I)
-            (normalBoolExpr (BImplies (sbInter 6I "a", sbInter 11I "b")))
-
-    [<Test>]
-    let ``getIntermediate on 'add' is max of the addends matching the name``() =
-        checkIntermediate "a" (Some 2I)
-            (normalIntExpr
-                (IAdd
-                    [ siInter 1I "a"
-                      siAfter "b"
-                      siInter 3I "b"
-                      siBefore "c"
-                      siInter 2I "a" ]))
-
-    [<Test>]
-    let ``getIntermediate on 'modulo' is max of its arguments matching the name`` () =
-        checkIntermediate "a" (Some 11I)
-            (normalIntExpr (IMod (siInter 6I "a", siInter 11I "a")))
