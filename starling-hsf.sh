4aba860ba0e702e843ac1127a20a893d0e1f6ff0
Matt Windsor <mattwindsor@btinternet.com>
2017-04-21 12:05:05 +0100

    Update HSF examples to new syntax
    Some of these were still broken WRT the last syntax change.
    
    A lot of these diverge on my computer and HSF version, so I can't check them fully.

--- starling-hsf.sh
+++ starling-hsf.sh
@@ -21,7 +21,17 @@ fi
 
 echo "--- STARLING ---"
 # no-smt-reduce is needed to give HSF enough contraints to be practical.
-./starling.sh -shsf -Bno-smt-reduce $* | tee $tempfile
+if ./starling.sh -shsf -Bno-smt-reduce $* > $tempfile;
+then
+	echo "Starling succeeded:"
+	cat $tempfile
+else
+	starling_exitc=$?
+	echo "Starling FAILED."
+    rm $tempfile
+	exit $starling_exitc
+fi
+
 echo "--- HSF ---"
 $QARMC -get-model $tempfile
 rm $tempfile
